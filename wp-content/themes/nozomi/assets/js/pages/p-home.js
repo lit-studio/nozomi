7230357e7581143aa4365d19eb0284bfd8fafb37
dima-tsaran-studio <dima@litnevski.studio>
2022-04-15 00:29:01 +0300

    footer and sections

--- wp-content/themes/nozomi/assets/js/pages/p-home.js
+++ wp-content/themes/nozomi/assets/js/pages/p-home.js
@@ -1,6 +1,12 @@
 import { gsap } from "gsap";
-import { DrawSVGPlugin } from "../../libs/gsap-shockingly-green/src/DrawSVGPlugin";
+import lottie from "lottie-web";
+import VimeoPlayer from '@vimeo/player';
+import {DrawSVGPlugin} from "../../libs/gsap-shockingly-green/src/DrawSVGPlugin";
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
 gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(ScrollTrigger);
+
 export default function () {
 
   console.log("home page");
@@ -122,5 +128,126 @@ export default function () {
     window.addEventListener("resize", newSizePerspective);
   }
   homePerspectiveFunc();
+  function btnLottieFunc() {
+    let btnPortfolio = document.querySelector(".btn-portfolio--js");
+    let btnAbout = document.querySelector(".btn-about--js");
+    let btnBook = document.querySelector(".btn-book--js");
+    if (!btnPortfolio.querySelector("svg")) {
+      function btnPortfolioHover() {
+        const btnPortfolioAnim = lottie.loadAnimation({
+          container: btnPortfolio,
+          renderer: "svg",
+          loop: false,
+          autoplay: false,
+          path: nozomiData.rootUrl + "/assets/lottie/button_portfolio.json"
+        });
+  
+        btnPortfolio.addEventListener("mouseenter", () => {
+          btnPortfolioAnim.playSegments([0, 30], true);
+        });
+  
+        btnPortfolio.addEventListener("mouseleave", () => {
+          btnPortfolioAnim.playSegments([31, 60], true);
+        });
+  
+  
+      }
+      if (btnPortfolio !== null) {
+        btnPortfolioHover();
+      }
+    }
+    if (!btnAbout.querySelector("svg")) {
+      function btnAboutHover() {
+        const btnAboutAnim = lottie.loadAnimation({
+          container: btnAbout,
+          renderer: "svg",
+          loop: false,
+          autoplay: false,
+          path: nozomiData.rootUrl + "/assets/lottie/button_about.json"
+        });
+  
+        btnAbout.addEventListener("mouseenter", () => {
+          btnAboutAnim.playSegments([0, 30], true);
+        });
+  
+        btnAbout.addEventListener("mouseleave", () => {
+          btnAboutAnim.playSegments([31, 60], true);
+        });
+  
+  
+      }
+      if (btnAbout !== null) {
+        btnAboutHover();
+      }
+    }
+    if (!btnBook.querySelector("svg")) {
+      function btnBookHover() {
+        const btnBookAnim = lottie.loadAnimation({
+          container: btnBook,
+          renderer: "svg",
+          loop: false,
+          autoplay: false,
+          path: nozomiData.rootUrl + "/assets/lottie/button_book.json"
+        });
+  
+        btnBook.addEventListener("mouseenter", () => {
+          btnBookAnim.playSegments([0, 30], true);
+        });
+  
+        btnBook.addEventListener("mouseleave", () => {
+          btnBookAnim.playSegments([31, 60], true);
+        });
+  
+  
+      }
+      if (btnBook !== null) {
+        btnBookHover();
+      }
+    }
+  }
+  btnLottieFunc();
+  function controlVideos() {
+    let $iframes = $('.video--js');
+    $iframes.each(function () {
+      let $this = $(this);
+
+
+    let $videoPlayerLayout =$this.find('.item-video--layout--js');
+    // let $currentIframeSrc;
+    let videoId = $this.attr('data-set-vimeo-id');
+    let vimeoPlayer = new VimeoPlayer($this,{
+      id: videoId,
+      width: '100%',
+      height: '100%',
+      controls:	false,
+      quality:	false
+    });
+
+
+    // vimeoPlayer.on('play', function() {
+    //     console.log('played the video!');
+    // });
+    // console.log('vimeo'); 
+    $videoPlayerLayout.hover(() => {
+      vimeoPlayer.play();
+    }, () => {
+      vimeoPlayer.pause();
+    });
+        // $videoPlayerLayout.on('click', function () {
+           
+        //     if (!$iframes.hasClass('played')) {
+        //         $iframes.addClass('played');
+        //         vimeoPlayer.play();
+
+        //     } else {
+        //         $iframes.removeClass("played");
+        //         vimeoPlayer.pause();
+
+        //     }
+
+        // });
+  });
+}
+controlVideos();
 
 }
