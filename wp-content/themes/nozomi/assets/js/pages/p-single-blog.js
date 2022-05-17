ed475c4e802cb80824368625d1b130b06cf67234
dima-tsaran-studio <dima@litnevski.studio>
2022-05-17 17:30:23 +0300

    share social

--- wp-content/themes/nozomi/assets/js/pages/p-single-blog.js
+++ wp-content/themes/nozomi/assets/js/pages/p-single-blog.js
@@ -2,4 +2,25 @@ export default function () {
     console.log("single blog");
     $('.blog-content-wrap p img').unwrap();
     $('.blog-content-wrap blockquote p').contents().unwrap();
+
+    function shareMobile() {
+
+        if (screen.width <= 1024) {
+            let $blogTopShare = $('.blog-top-share');
+            let $blogTopShareLi = $blogTopShare.find('.blog-top-share-li');
+            let $blogTopShareBtn = $blogTopShare.find('.blog-top-share-mobile--js');
+            $blogTopShareBtn.on("click", function () {
+                if (!$blogTopShareBtn.hasClass('close')) {
+                    $blogTopShareBtn.addClass('close');
+                    $blogTopShareLi.addClass('open');
+                }
+                else{
+                    $blogTopShareBtn.removeClass('close');
+                    $blogTopShareLi.removeClass('open');
+                }
+            });
+        }
+    }
+    shareMobile();
+
 }
