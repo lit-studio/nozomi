dd2771368195c59c244b26735ce0f90da498ddee
dima-tsaran-studio <dima@litnevski.studio>
2022-06-06 17:17:01 +0300

    about page

--- wp-content/themes/nozomi/page-portfolio.php
+++ wp-content/themes/nozomi/page-portfolio.php
@@ -30,17 +30,6 @@ get_header();
                                 </button>
                             </div>
                             <div class="nav-menu nav-menu--js">
-                            <!-- <ul>
-                            <li class='menu-item menu-item-type-custom menu-item-object-custom  '>
-                                <a href="/">All works</a>
-                            </li>
-                            <li class='menu-item menu-item-type-custom menu-item-object-custom  '>
-                                <a href="/">All works</a>
-                            </li>
-                            <li class='menu-item menu-item-type-custom menu-item-object-custom  '>
-                                <a href="/">All works</a>
-                            </li>
-                            </ul> -->
                                 <?php 
                                 $curr_id = get_queried_object()->term_id;
                                 $terms = get_terms('portfolio_cat');
@@ -149,42 +138,6 @@ get_header();
                     </section>
                 </div> 
             </div>
-            <!-- <section class="nav">
-                <div class="nav-wrap">
-                    <div class="nav-mobile-btns">
-                        <button class='nav-mobile-btn nav-mobile-btn--js'><span class='nav-mobile-btn-text'>All posts</span>
-                        </button>
-                    </div>
-                    <div class="nav-menu nav-menu--js">
-                        <?php
-                        $curr_id = get_queried_object()->term_id;
-                        $terms = get_terms('portfolio_cat');
-
-                        if ($terms && !is_wp_error($terms)) {
-                            echo "<ul>";
-                            ?>
-                            <li class='menu-item menu-item-type-custom menu-item-object-custom'><a
-                                        href="<?php echo get_permalink(9); ?>">
-                                    All works</a></li>
-                            <?php
-
-                            foreach ($terms as $term) {
-
-                                ?>
-                                <li class='menu-item menu-item-type-custom menu-item-object-custom <?php if ($term->term_id == $curr_id) { ?> current-menu-item<?php } ?> '>
-                                    <a href="<?php echo get_term_link($term); ?>"><?php echo $term->name; ?></a>
-                                </li>
-
-                            <?php }
-
-                            echo "</ul>";
-                        }
-                        ?>
-                    </div>
-                    <div class="nav-menu-bg nav-menu-bg--js"></div>
-                </div>
-            </section> -->
-        </div>
         </div>
     </main><!-- #main -->
 
